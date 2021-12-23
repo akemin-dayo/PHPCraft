db49e7f24e3f46016981526b6e61ca63174e49b9
Karen/あけみ <karen@akemi.ai>
2021-12-22 21:02:42 -0800

    StreamWrapper: Made note of several types that appear to be handled incorrectly by StreamWrapper, heavily reorganised the code structure to make it more easily understandable, added convenience aliases for `byte` and `short` data types, and added operations for `float` data types.

--- src/PHPCraft/Core/Networking/StreamWrapper.php
+++ src/PHPCraft/Core/Networking/StreamWrapper.php
@@ -9,7 +9,6 @@ namespace PHPCraft\Core\Networking;
 use PHPCraft\Core\Helpers\Hex;
 use PHPCraft\Core\Helpers\Logger;
 
-// https://www.php.net/manual/en/function.pack.php
 // https://stackoverflow.com/questions/16039751/php-pack-format-for-signed-32-int-big-endian
 define('BIG_ENDIAN', pack('L', 1) === pack('N', 1));
 
@@ -65,84 +64,150 @@ class StreamWrapper {
 		$this->streamBuffer = [];
 	}
 
+	/* Type Mappings begin here */
+	// PHP pack() format reference: https://www.php.net/manual/en/function.pack.php
+
+	/*
+		byte (signed 8-bit integer)
+		PHP pack() format: signed char
+	*/
 	public function readInt8() {
 		return unpack("c", $this->read(1))[1];
 	}
-
 	public function writeInt8($data) {
 		return pack("c", $data);
 	}
+	// Convenience aliases
+	public function readByte() {
+		return $this->readInt8();
+	}
+	public function writeByte($data) {
+		return $this->writeInt8($data);
+	}
+	/* ******************************** */
 
+	/*
+		bool
+		PHP pack() format: signed char
+	*/
 	public function readBool() {
-		return (bool) $this->readInt8();
+		return (bool)$this->readInt8();
 	}
-
 	public function writeBool($data) {
-		if ($data == true) {
-			$this->writeInt8(0x01);
-		} else {
-			$this->writeInt8(0x00);
-		}
+		return $this->writeInt8(((bool)$data) ? 0x01 : 0x00);
 	}
+	/* ******************************** */
 
+	/*
+		short (signed 16-bit integer)
+		PHP pack() format: unsigned short (always 16 bit, big endian byte order)
+
+		TODO (Karen) / WARNING: The pack() format being used here seems incorrect! (sign)
+	*/
 	public function readInt16() {
 		return unpack("n", $this->read(2))[1];
 	}
-
 	public function writeInt16($data) {
-		return pack("n*", $data);
+		return pack("n", $data);
 	}
+	// Convenience aliases
+	public function readShort() {
+		return $this->readInt16();
+	}
+	public function writeShort($data) {
+		return $this->writeInt16($data);
+	}
+	/* ******************************** */
 
+	/*
+		int (signed 32-bit integer)
+		PHP pack() format for READ: unsigned long (always 32 bit, big endian byte order)
+		PHP pack() format for WRITE: signed long (always 32 bit, machine byte order)
+
+		TODO (Karen) / WARNING: The READ pack() format being used here seems incorrect! (sign)
+	*/
 	public function readInt() {
 		return unpack("N", $this->read(4))[1];
 	}
-
 	public function writeInt($data) {
 		if (BIG_ENDIAN) {
 			return pack('l', $data);
 		}
-
-		return strrev(pack("l*", $data));
+		return strrev(pack("l", $data));
 	}
+	/* ******************************** */
 
+	/*
+		long (signed 64-bit integer)
+		PHP pack() format: signed long long (always 64 bit, machine byte order)
+
+		TODO (Karen) / WARNING: These functions do not seem to handle endianness correctly!
+	*/
 	public function readLong() {
 		return unpack("q", $this->read(8))[1];
 	}
-
 	public function writeLong($data) {
-		return pack("q*", $data);
+		return pack("q", $data);
 	}
+	/* ******************************** */
 
-	public function readString16() {
-		$l = $this->readInt16();
-		$str = "";
+	/*
+		float (32-bit float, single-precision)
+		PHP pack() format: float (machine dependent size and representation)
 
-		for ($i = 0; $i < $l; $i++) {
-			$str = $str . chr($this->readInt16());
+		※ NOTE: PHP 7.0.15+ and 7.1.1+ (both 2017/01/19) added the "G" format, which is "float (machine dependent size, big endian byte order)"
+			Consider switching to that instead.
+	*/
+	public function readFloat() {
+		return unpack("f", strrev($this->read(4)))[1];
+	}
+	public function writeFloat($data) {
+		if (BIG_ENDIAN) {
+			return pack("f", $data);
 		}
-
-		if (strlen($str) > 0) {
-			return $str;
-		} else {
-			// No string found?
-		}
+		return strrev(pack("f", $data));
 	}
+	/* ******************************** */
 
-	public function writeString16($str) {
-		$str = iconv("UTF-8", "UTF-16BE", $str);
+	/*
+		double (64-bit float, double-precision)
+		PHP pack() format: double (machine dependent size and representation)
 
-		return $str;
-	}
-
+		※ NOTE: PHP 7.0.15+ and 7.1.1+ (both 2017/01/19) added the "E" format, which is "double (machine dependent size, big endian byte order)"
+			Consider switching to that instead.
+	*/
 	public function readDouble() {
 		return unpack("d", strrev($this->read(8)))[1];
 	}
-
 	public function writeDouble($data) {
 		if (BIG_ENDIAN) {
-			return pack("d*", $data);
+			return pack("d", $data);
 		}
-
 		return strrev(pack("d", $data));
 	}
+	/* ******************************** */
+
+	/*
+		string16
+		Reference: https://wiki.vg/index.php?title=Protocol&oldid=510#Data_Types
+	*/
+	public function readString16() {
+		$expectedStringLength = $this->readShort();
+		$constructedString = "";
+
+		for ($i = 0; $i < $expectedStringLength; $i++) {
+			$constructedString = $constructedString . chr($this->readShort());
+		}
+
+		if (strlen($constructedString) > 0) {
+			return $constructedString;
+		} else {
+			// TODO (Karen): Add proper error handling for when the string somehow ends up empty.
+		}
+	}
+	public function writeString16($str) {
+		$str = iconv("UTF-8", "UTF-16BE", $str);
+		return $str;
+	}
+	/* ******************************** */
 }
